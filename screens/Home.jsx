a8b8ba2de1b20a397ab8ac05746d59f46f924967
DeveloperYamin <developer.yamin@gmail.com>
2022-04-03 14:20:44 +0600

    creating SearchBar component

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Platform, SafeAreaView, StyleSheet, View } from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
-const Home = () => {
+export default function Home() {
   return (
-    <View>
-      <HeaderTabs/>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+    </SafeAreaView>
   );
-};
+}
 
-export default Home;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eee",
+    paddingTop: Platform.OS === "android" ? 30 : 0,
+  },
+});
